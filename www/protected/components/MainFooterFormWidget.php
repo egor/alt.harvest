1c5761b1164803af6bea7add881e5d131364f029
Egor Rihnov <egor.developer@gmail.com>
2013-02-05 11:30:40 +0400

    формы на главной

---
+++ www/protected/components/MainFooterFormWidget.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description of MainHeaderPortlet
+ *
+ * @author egorik
+ */
+
+class MainTopFormWidget extends CWidget
+{
+    public $footer = 0;
+    public function init()
+    {
+        $this->render('mainTopFormWidget',array());
+    }
+ 
+
+}
+
+?>
