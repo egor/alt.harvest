e3434346dbd92942bc714b36bb7d7f4037056001
Egor Rihnov <egor.developer@gmail.com>
2013-02-07 13:17:22 +0400

    Вывод наших работ, без верстки

--- www/protected/controllers/WorksController.php
+++ www/protected/controllers/WorksController.php
@@ -1,36 +1,34 @@
 <?php
+/**
+ * WorksController
+ * Модуль "Наши работы"
+ * 
+ * @author Egor Rihnov <egor.developer@gmail.com>
+ * @version 1.0
+ * @package frontEnd
+ */
+class WorksController extends Controller {
 
-class WorksController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
-
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+    /**
+     * Список работ
+     */
+    public function actionIndex() {
+        $worksData = Pages::model()->findByPk(Yii::app()->params['modules']['works']);
+        $worksCount = Reviews::model()->count('visibility=:visibility', array(':visibility' => 1));
+        $paginator = new CPagination($worksCount);
+        $setting = Settings::model()->findByPk(6);
+        $paginator->pageSize = $setting->value;
+        $paginator->route = '/' . $worksData->url;
+        $countPage = ceil($worksCount / $setting->value);
+        if (isset($_GET['page'])) {
+            $start = ((int) ($_GET['page']) - 1) * $paginator->pageSize;
+        } else {
+            $start = 0;
+        }
+        $this->pageTitle = $worksData->meta_title;
+        Yii::app()->clientScript->registerMetaTag('meta_keywords', $worksData->meta_keywords);
+        Yii::app()->clientScript->registerMetaTag('meta_keywords', $worksData->meta_description);
+        $model = Works::model()->findAll('visibility=:visibility ORDER BY `date` DESC LIMIT ' . $start . ', ' . $setting->value, array(':visibility' => 1));
+        $this->render('index', array('model' => $model, 'worksData' => $worksData, 'paginator' => $paginator, 'countPage' => $countPage, 'settingValue' => $setting->value));
+    }
 }
--- www/protected/views/works/index.php
+++ www/protected/views/works/index.php
@@ -1,13 +1,51 @@
+<link rel="stylesheet" type="text/css" href="/css/site/paginator3000.css" />
+<script type="text/javascript" src="/js/site/paginator3000.js"></script>
 <?php
 /* @var $this WorksController */
-
-$this->breadcrumbs=array(
-	'Works',
+$this->breadcrumbs = array(
+    $worksData->menu_name,
 );
 ?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+<h1><?php echo $worksData->h1; ?></h1>
+<?php
+echo $worksData->text;
+echo '<div class="works-list">';
+$z = 0;
+$count = count($model);
+foreach ($model as $value) {
+    $z++;
+    echo '<div class="works-item">';
+    echo '<div class="works-item-date">' . 
+            date('d.m.Y', $value->date) . 
+            ' | &nbsp;</div><h2>' .
+            $value->menu_name . 
+            '</h2><br clear="all"/>';
+    echo '<div class="works-item-short-text">' . 
+            (!empty($value->img) ? '<a href="/images/works/big/' . $value->img_big . '"><img class="works-list-img" src="/images/works/' . $value->img . '" alt="' . $value->img_alt . '" title="' . $value->img_title . '" /></a>' : '') .
+            $value->text .
+            '</div>';
+    echo '<div class="works-user-data">'.$value->address.'</div></div>';
+    if ($z < $count) {
+        echo '<div class="footer-list-line"></div>
+            <div class="footer-list-line2"></div>';
+    }
+}
+?>
+</div>
+<div class="paginator" id="paginator_page">
+<?php
+$this->widget('CLinkPager', array(
+    'pages' => $paginator,
+    'id' => ''
+));
+?>
+</div>
+<script type="text/javascript">
+paginator_example = new Paginator(
+    "paginator_page", // id контейнера, куда ляжет пагинатор
+    <?php echo $countPage; ?>, // общее число страниц
+    <?php echo $settingValue; ?>, // число страниц, видимых одновременно
+    <?php echo (isset($_GET['page']) ? $_GET['page'] : 1); ?>, // номер текущей страницы
+    "<?php echo '/' . $worksData->url . '/'; ?>"// url страниц
+);
+</script>
