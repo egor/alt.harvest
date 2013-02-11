71b53395584937fdb6d1b7f5f2ccc67276e52b9d
Egor Rihnov <egor.developer@gmail.com>
2013-02-11 13:49:08 +0200

    проверка форм в акции

--- www/protected/views/stock/index.php
+++ www/protected/views/stock/index.php
@@ -21,10 +21,10 @@ foreach ($model as $value) {
     echo '<div class="stock-item-date"></div><h2>' . $value->menu_name . '</h2>';
     echo '<br clear="all"/>
         <div class="stock-img" ' . (!empty($value->img) ? 'style="background:url(/images/stock/' . $value->img . ');"' : '').'>
-<form action="/zayavka-otpravlena" method="post">
+<form action="/zayavka-otpravlena" id="formz'.$value->stock_id.'" method="post">
 <div class="stock-form">
-<div class="stock-input-str-name"><span>Ваше имя:</span><input name="name" class="stock-input-name"></div>
-<div class="stock-input-str-phone"><span>Ваш телефон:</span><input name="phone" class="stock-input-phone"></div>
+<div class="stock-input-str-name"><span>Ваше имя:</span><input name="name" id="name'.$value->stock_id.'" class="stock-input-name"></div>
+<div class="stock-input-str-phone"><span>Ваш телефон:</span><input name="phone" id="phone'.$value->stock_id.'" class="stock-input-phone"></div>
 <div class="stock-submit-str"><input type="submit" value="" class="main-top-form-sub2"></div>
 <div id="countdown'.$value->stock_id.'"></div>   
 <div class="countdown-str"><span class="day">дней</span><span class="hour">часов</span><span class="min">минут</span><span class="sec">секунд</span></div>
@@ -71,20 +71,73 @@ if ($countPage>1) {
 <!-- JavaScript -->		
 		<script src="/library/countdown/jquery.countdown.js"></script>
                 <script>
+                    
+                    
+                    $(document).ready(function(){
+                        <?php
+                        foreach ($model as $value) {
+                        ?>
+                        if ($("#name<?php echo $value->stock_id; ?>").val() == 'Введите Ваше имя') {
+                            $("#name<?php echo $value->stock_id; ?>").val('');
+                        }
+                        if ($("#phone<?php echo $value->stock_id; ?>").val() == 'Введите Ваш телефон') {
+                            $("#phone<?php echo $value->stock_id; ?>").val('');
+                        }
+                        $("#formz<?php echo $value->stock_id; ?>").submit(function() {
+                            ret = true;
+                            n = $("#name<?php echo $value->stock_id; ?>").val();
+                            p = $("#phone<?php echo $value->stock_id; ?>").val();
+                            if (n == '' || n == 'Введите Ваше имя') {
+                                $("#name<?php echo $value->stock_id; ?>").val('Введите Ваше имя');
+                                $("#name<?php echo $value->stock_id; ?>").css('color', 'red');
+                                ret = false;
+                            }
+                            if (p == '' || p == 'Введите Ваш телефон') {
+                                $("#phone<?php echo $value->stock_id; ?>").val('Введите Ваш телефон');
+                                $("#phone<?php echo $value->stock_id; ?>").css('color', 'red');
+                                ret = false;
+                            }
+                            return ret;
+                        })
+                        
+                        $("#name<?php echo $value->stock_id; ?>").click(function() {
+                            if ($("#name<?php echo $value->stock_id; ?>").val() == 'Введите Ваше имя') {
+                                $("#name<?php echo $value->stock_id; ?>").val('');
+                                $("#name<?php echo $value->stock_id; ?>").css('color', 'black');
+                            }
+                        })
+                        
+                        $("#phone<?php echo $value->stock_id; ?>").click(function() {
+                            if ($("#phone<?php echo $value->stock_id; ?>").val() == 'Введите Ваш телефон') {
+                                $("#phone<?php echo $value->stock_id; ?>").val('');
+                                $("#phone<?php echo $value->stock_id; ?>").css('color', 'black');
+                            }
+                        })
+                        
+                        $("#phone<?php echo $value->stock_id; ?>").keydown(function(event) {
+                            // Разрешаем: backspace, delete, tab и escape
+                            if (event.keyCode == 13 || event.keyCode == 32 || event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+                            // Разрешаем: Ctrl+A
+                            (event.keyCode == 65 && event.ctrlKey === true) ||
+                            // Разрешаем: home, end, влево, вправо
+                            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                            // Ничего не делаем
+                            return;
+                        }
+                            else {
+                                // Обеждаемся, что это цифра, и останавливаем событие keypress
+                                if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                                    event.preventDefault();
+                                }  
+                            }
+                        });
+                        <?php
+                        }
+                        ?>
+                    })
+                    
+                    
                     $(function(){
-	/*
-	var note = $('#note'),
-		ts = new Date(2014, 0, 1),
-		newYear = true;
-	
-	if((new Date()) > ts){
-		// Задаем точку отсчета для примера. Пусть будет очередной Новый год или дата через 10 дней.
-		// Обратите внимание на *1000 в конце - время должно задаваться в миллисекундах
-		ts = (new Date()).getTime() + 10*24*60*60*1000;
-		newYear = false;
-	}*/
-		//ts = new Date(2013, 1, 11, 0,0,0),
-                //alert(ts);
                 <?php
                 foreach ($model as $value) {
                     $datetime1 = date_create(date('Y-m-d'));
